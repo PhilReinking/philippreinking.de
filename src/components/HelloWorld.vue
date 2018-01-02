1dd62b4d6dd5c81274e59b5ae77b61e3dc18fd0c
Unknown <mail@philippreinking.de>
2018-01-02 12:15:14 +0100

    add scaffold

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="page-grid mx-auto">
+        <div class="bg-grey-100 text-white flex justify-center items-center">
+            <a href="/">
+                <logo></logo>
+            </a>
+        </div>
+
+        <div class="col-span-3 py-8 px-6 flex justify-center items-center relative overflow-hidden">
+            <div class="bg-blue absolute pin-t pin-l pin-r pin-b skyline" v-perspective>
+            </div>
+            <div class="text-white z-10">
+                <h1 class="font-heading font-black text-5xl">Hello, I'm Philipp.
+                    <span class="text-teal-500">I write code.</span>
+                </h1>
+                <p class="mt-4 text-lg leading-loose max-w-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis cum perspiciatis quia consequatur quas, facere quisquam rerum dolor non vero odit, ut veritatis, repellat odio. Quae eligendi et id ex?</p>
+            </div>
+        </div>
+
+        <div class="bg-red col-span-3 row-span-2">
+            cool
+        </div>
+
+        <div class="bg-grey-100 flex flex-col items-center justify-center">
+            <a class="text-xl text-grey-700 hover:text-blue-500 no-underline py-3" href="#">Consulting</a>
+            <a class="text-xl text-grey-700 hover:text-blue-500 no-underline py-3" href="#">Projects</a>
+            <a class="text-xl text-grey-700 hover:text-blue-500 no-underline py-3" href="#">Blog</a>
+            <a class="text-xl text-grey-700 hover:text-blue-500 no-underline py-3" href="#">Contact</a>
+        </div>
+        <div class="bg-green">
+
+        </div>
+        <div class="bg-blue">
+
+        </div>
+    </div>
+</template>
+
+<script>
+import Logo from "./Logo.vue"
+
+import perspective from "../directives/perspective"
+
+export default {
+    name: "HelloWorld",
+
+    components: {
+        Logo,
+    },
+
+    directives: {
+        perspective,
+    },
+}
+</script>
---
+++ src/directives/perspective.js
@@ -0,0 +1,24 @@
+export default {
+    bind(el) {
+        console.log("bound", el)
+
+        // set initial transform styles
+        el.style.transform = `scale(1.05)`
+
+        window.addEventListener("mousemove", e => {
+            window.requestAnimationFrame(timestamp => {
+                let yShift = e.clientY / e.view.innerHeight
+                let xShift = e.clientX / e.view.innerWidth
+
+                const BASE = 3
+
+                let yShiftAbsolute = BASE * yShift
+                let xShiftAbsolute = BASE * xShift
+
+                let transform = `perspective(1000px) rotate3d(1,0,0,${yShiftAbsolute}deg) rotate3d(0,1,0,${xShiftAbsolute}deg) scale(1.05)`
+
+                el.style.transform = transform
+            })
+        })
+    },
+}
